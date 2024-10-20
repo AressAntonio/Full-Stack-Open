83eff6d66ca7c93c79103906fdb78a63f30066da
Antonio_Garrido <99376135+AressAntonio@users.noreply.github.com>
2024-10-20 00:10:38 -0600

    1.2: Información del Curso, paso 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -17,23 +17,39 @@ const content = [
   {part: 'Using props to pass data', exercises: 7},
   {part: 'State of a component', exercises: 14}
 
-]
+];
+
+//Part component
+const Part = ()=>{
+  return(
+    <p>
+        {content[0].part} whit {content[0].exercises} exercises.
+    </p>
+  )
+}
+
+const Part2 = ()=>{
+  return(
+    <p>
+        {content[1].part} whit {content[1].exercises} exercises.
+    </p>
+  )
+}
+const Part3 = ()=>{
+  return(
+    <p>
+        {content[2].part} whit {content[2].exercises} exercises.
+    </p>
+  )
+}
 
 //Content component
 const Content = ()=>{
   return(
     <>
-      <p>
-        {content[0].part} whit {content[0].exercises} exercises.
-      </p>
-
-      <p>
-        {content[1].part} whit {content[1].exercises} exercises
-      </p>
-
-      <p>
-        {content[2].part} whit {content[2].exercises} exercises
-      </p>
+      <Part />
+      <Part2 />
+      <Part3 />
     </>
     
   )
