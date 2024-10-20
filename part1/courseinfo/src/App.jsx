f11f77ca9e04ede3849b5bb5ea2614597ef7f9a7
Antonio_Garrido <99376135+AressAntonio@users.noreply.github.com>
2024-10-20 15:07:17 -0600

    1.3: Información del Curso, paso 3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,78 +1,88 @@
-//Header component
-const Header = ()=>{
-  const course = 'Half Stack application development';
-
-  return(
-    <>
-      <h1>
-      <strong>{course}</strong>
-      </h1>
-    </>
-    
-  )
-};
-
-const content = [
-  {part: 'Fundamentals of React', exercises: 10},
-  {part: 'Using props to pass data', exercises: 7},
-  {part: 'State of a component', exercises: 14}
-
-];
-
-//Part component
-const Part = ()=>{
-  return(
-    <p>
-        {content[0].part} whit {content[0].exercises} exercises.
-    </p>
-  )
-}
-
-const Part2 = ()=>{
-  return(
-    <p>
-        {content[1].part} whit {content[1].exercises} exercises.
-    </p>
-  )
-}
-const Part3 = ()=>{
-  return(
-    <p>
-        {content[2].part} whit {content[2].exercises} exercises.
-    </p>
-  )
-}
-
-//Content component
-const Content = ()=>{
-  return(
-    <>
-      <Part />
-      <Part2 />
-      <Part3 />
-    </>
-    
-  )
-}
-
-//Total component
-const Total = ()=>{
-  return(
-    <>
-      <p>
-        Number of exercises {content[0].exercises + content[1].exercises + content[2].exercises}
-      </p>
-    </>
-  )
-}
 
 const App = ()=>{
   
+  //variables
+  const course = 'HAlf Stack application development';
+
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  };
+
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14.
+  };
+
+  //HEADER COMPONENT
+  const Header = ()=>{
+    return(
+
+      <h1>
+      <strong>{course}</strong>
+      </h1>
+    )
+  };
+
+  //CONTENT COMPONENT
+  const Part1 = ()=>{
+    return(
+
+      <p>
+        {part1.name} whit {part1.exercises} exercises.
+      </p>
+
+    )
+  };
+
+  const Part2 = ()=>{
+    return(
+      <p>
+        {part2.name} whit {part2.exercises} exercises.
+      </p>
+    )
+  };
+
+  const Part3 = ()=>{
+    return(
+      <p>
+        {part3.name} whit {part3.exercises} exercises.
+    </p>
+    )
+  };
+
+  const Content = ()=>{
+    return(
+
+      <>
+        <Part1 />
+        <Part2 />
+        <Part3 />
+      </>
+      
+    )
+  }
+
+  //TOTAL COMPONENT
+  const Total = ()=>{
+    return(
+      <>
+        <p>
+          Number of exercises: {part1.exercises + part2.exercises + part3.exercises}
+        </p>
+      </>
+    )
+  }
 
   return(
 
     <>
-      <Header />
+      <Header course={course}/>
       <Content />
       <Total />
     </>
