01332891ebc5500dcd96724d3fb2c083c17b34d7
Antonio_Garrido <99376135+AressAntonio@users.noreply.github.com>
2024-10-28 18:30:09 -0600

    1.7: unicafe, paso 2 and 1.8: unicafe, paso 3

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+
+
 const Header = (props)=>{
   return(
     <h1><strong>{props.text}</strong></h1>
@@ -10,17 +12,76 @@ const Button = (props) => (
   </button>
 )
 
-const Statistics = (props)=>{
-  return(
-    <>
-    <h2><strong>{props.text}</strong></h2>    
-    </>
-  )
+
+
+const Promedio = ({bad,good,neutral})=>{
   
+  let TotalPromGood = 0;
+  let TotalPromBab = 0;
+  let TotalPromNeutro = 0;
+  let TotalProm;
+
+  if(bad){
+    TotalPromBab = good - bad;
+
+  }else if(good){
+    TotalPromGood = good; 
+  
+  }else if(neutral){
+    TotalPromNeutro = 0;
+    
+  }
+  let Prom = TotalPromGood + TotalPromBab + TotalPromNeutro;
+  TotalProm = (TotalPromGood + TotalPromBab + TotalPromNeutro) / 3;
+  console.log('Promedio: ',Prom,'\r PorcenProm: ', TotalProm);
+
+  return(
+    <strong>
+      Average: {TotalProm} %
+    </strong>
+  )
+}
+
+
+const Statistics = ({start,bad,neutral,good})=>{
+  if (!neutral&!bad&!good){
+    return(
+      <>
+        <h2>Statistics</h2>
+        <p>{start}</p>
+      </>
+    )
+  }else {
+    const Total = bad + neutral + good;
+    const prom = Total / 3;
+
+    const porc = good ? good / Total * 100 : '0';
+ 
+   return(
+    <>
+      <h2><strong>Statistics</strong></h2>
+      <p><strong>Good: {good}</strong></p>
+      <p><strong>Neutral: {neutral}</strong></p>
+      <p><strong>Bad: {bad}</strong></p>
+      <p><strong>All: {Total}</strong></p>
+      <p>
+        <Promedio bad={bad} good={good} neutral={neutral}/>
+      </p>
+      <p>
+        <strong>
+          Positive: {porc} %
+        </strong>
+      </p>
+        
+    </>
+   )
+
+  }
 }
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
+  const statics = 'statistics';
   const [good, setGood] = useState(0);
   const setToGood = (newValue)=>{
     console.log('value now of goob', newValue)
@@ -39,6 +100,12 @@ const App = () => {
     setBad(newValue)
   }
 
+  const [start, setDatos] = useState('No feedback given');
+  const setToDatos=(newValue)=>{
+    console.log('inicial rendering', start)
+    setDatos(newValue)
+  }
+
   return (
     <>
 
@@ -50,21 +117,7 @@ const App = () => {
       
       <Button handleClick={()=>setToBad(bad + 1)} text='Bad' />
       
-      <Statistics text='statistics' />
-
-      <p>
-      <strong>
-      <span>Good: {good}</span>
-      </strong>
-      </p>
-      <p>
-      <span><strong>
-      Neutral: {neutral}</strong></span>
-      </p>
-      <p>
-      <span><strong>
-      Bad: {bad}</strong></span>
-      </p>
+      <Statistics start={start} bad={bad} good={good} neutral={neutral}/>
       
     </>
   )
