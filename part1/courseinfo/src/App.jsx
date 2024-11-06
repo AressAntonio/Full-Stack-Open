22696219a9af5f54d4f084635c69efc731a70325
Antonio_Garrido <99376135+AressAntonio@users.noreply.github.com>
2024-11-05 21:31:03 -0600

    2.2: Información del curso paso 7,2.3*: Información del curso paso 8

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,7 +9,22 @@ const Course = ({course})=>{
           </p>
       );
     });
-    console.log(partDetails);
+    console.log('objects render: ',partDetails);
+
+    const Sum = ()=>{
+      const allExercises = course.parts.reduce((acount, part)=>{
+        return acount += part.exercises;
+      }, 0)
+      console.log('total of exercises: ',allExercises);
+
+      return(
+        <p>
+           <strong>
+            All exercises:  {allExercises}.
+            </strong>
+        </p>
+      )
+    }
 
     return(
       <>
@@ -21,6 +36,7 @@ const Course = ({course})=>{
 
         <article>
           {partDetails}
+          <Sum />
         </article>
       </>
     )
@@ -48,7 +64,12 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
-      }
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      },
     ]
   }
 
