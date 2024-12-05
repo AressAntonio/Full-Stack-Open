85fa19b8db7f09f33ea6e0b0d088b84811c52184
Antonio_Garrido <99376135+AressAntonio@users.noreply.github.com>
2024-12-04 22:20:52 -0600

    Filtrado de elementos mostrados

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,13 +1,40 @@
+import { useState } from "react";
 
 import Note from "./components/Note";
 
 
-const App = ({notes})=>{
+const App = (props)=>{
+
+  const [notes, setNotes] = useState(props.notes);
+
+  const [newNote, setNewNotes] = useState('a new note...');
+
+  const [showAll, setShowAll] = useState(true);
 
   console.log('this is a objecs of main.jsx: ',notes);
 
+  const noteToShow = showAll ? notes : notes.filter(note=>note.important === true);
+
   const result = notes.map(note=>note.id);
   console.log(result);
+
+  const addNote = (event)=>{
+    event.preventDefault();
+    const noteObject ={
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNotes('')
+    
+    console.log('Button clicked', event.target);
+  };
+
+  const handleNoteChange = (event)=>{
+    console.log(event.target.value);
+    setNewNotes(event.target.value);
+  };
 
 
   return(
@@ -15,13 +42,24 @@ const App = ({notes})=>{
     <div>
       <h1><strong>Notes</strong></h1>
 
+      <div>
+        <button onClick={()=>setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+
       <ul>
        {
-        notes.map(note=>
+        noteToShow.map(note=>
          <Note key={note.id} note={note} />
         )
        }
       </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
 
     </div>
     
@@ -30,32 +68,5 @@ const App = ({notes})=>{
   
   
 }
-
-/*const Course = (props)=>{
- 
-  console.log(props);
-
-  cosnt {course} = props
-
-  return(
-    <div>
-    <Header course={course} />
-    </div>
-  )
-}
-
-const App = ()=>{
-  const course = {
-
-  }
-
-  console.log('App works...')
-
-  return(
-    <div>
-      < Course course={course} />
-    </div>
-  )
-}*/
 
 export default App
